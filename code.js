a77f43b034b4a6e558bc0441f142c8a7bbd3205e
azheng249 <andy.zheng249@gmail.com>
2016-04-23 16:46:22 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function favIceCream(icecream){
+
+  return "I love " + icecream;
+}
+
+function shouting(thing){
+  return thing.toUpperCase();
+}
+
+function roundDown (number) {
+  return Math.floor(number);
+}
+
+function theTruth () {
+  return true;
+}
